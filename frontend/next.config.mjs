36c7a5e2701a23b063f525e108f48b628cd06288
Anjori Sarabhai <137786845+anjorisarabhai@users.noreply.github.com>
2025-12-30 22:51:18 +0530

    Add basePath and assetPrefix for deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,15 @@
+const repoName = "SatarkMitra-Disaster-Management-System";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
