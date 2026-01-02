26b1da6d22be147cceddf881a699e02478eaa89f
Anjori Sarabhai <anjori3005@gmail.com>
2026-01-02 22:10:29 +0530

    for deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,17 +1,4 @@
-const repoName = "SatarkMitra-Disaster-Management-System";
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
-
-  trailingSlash: true,   // 🔴 THIS IS THE MISSING PIECE
-
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
