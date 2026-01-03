d8549dc4bdfb2d0b873ae9ea83b0025b5d7167e2
vaanii-18 <tyagivaani18@gmail.com>
2026-01-03 20:48:48 +0530

    app.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FloodAlertPanel from './components/FloodAlertPanel';
+
+function App() {
+  return (
+    <div className="App bg-gray-100 min-h-screen flex items-center justify-center">
+      <FloodAlertPanel />
+    </div>
+  );
+}
+
+export default App;
