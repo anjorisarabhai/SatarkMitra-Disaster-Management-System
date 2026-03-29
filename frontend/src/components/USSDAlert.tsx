821c70b260b256f7d97c792b61531a284b849653
vaanii-18 <tyagivaani18@gmail.com>
2026-03-29 12:14:53 +0530

    USSDAlert component created

---
+++ frontend/src/components/USSDAlert.tsx
@@ -0,0 +1,18 @@
+export default function USSDAlert() {
+  return (
+    <div className="bg-yellow-100 border border-yellow-300 p-3 rounded-lg text-sm flex items-center justify-between">
+      
+      <span>
+        📞 No internet? Dial <b>*123#</b> for offline support
+      </span>
+
+      <a
+        href="tel:*123%23"
+        className="text-blue-600 font-semibold underline"
+      >
+        Dial Now
+      </a>
+
+    </div>
+  );
+}
