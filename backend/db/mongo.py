17e633b9aeb3eaf77c35d9884a9dcf3eb70ecc9a
Anjori Sarabhai <anjori3005@gmail.com>
2026-03-29 19:11:39 +0530

    for alerts and users collection

--- backend/db/mongo.py
+++ backend/db/mongo.py
@@ -8,6 +8,16 @@ MONGO_URI = os.getenv("MONGO_URI")
 
 mongo_client = MongoClient(MONGO_URI)
 
+# 📦 Database
 db = mongo_client["satarkmitra"]
 
+# =========================
+# 📁 COLLECTIONS
+# =========================
+
 citizen_reports = db["citizen_reports"]
+
+# ✅ NEW COLLECTIONS
+users_collection = db["users"]
+alerts_collection = db["alerts"]
+subscribers_collection = db["subscribers"]
